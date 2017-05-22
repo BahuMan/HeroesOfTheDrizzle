2b962838d93f50c0d9cd01893cb49df18d46ab8a
Bart Huylebroeck <BahuMan@users.noreply.github.com>
2017-05-22 13:28:36 +0200

    fixed button timer indicator

--- Assets/GUI/HeroGUI.cs
+++ Assets/GUI/HeroGUI.cs
@@ -9,15 +9,27 @@ public class HeroGUI : MonoBehaviour {
     //references
     [SerializeField]
     private Texture2D Ability2Cursor;
-    
     [SerializeField]
     private Dropdown _levelDropdown;
     [SerializeField]
     private Slider _HealthSlider;
     [SerializeField]
     private Slider _ManaSlider;
+    [SerializeField]
+    private Button HearthStoneButton;
+    [SerializeField]
+    private Button Ability1Button;
+    [SerializeField]
+    private Button Ability2Button;
+
 
     //private
+    private Image HearthStoneButtonImg;
+    private Image Ability1ButtonImg;
+    private Image Ability2ButtonImg;
+    private Image HealthFillImg;
+    private Color HealthFillColor = Color.black;
+
     private HeroControl _localHero;
     private enum GUIStatus { NORMAL, ABILITY2SELECT }
     private GUIStatus status = GUIStatus.NORMAL;
@@ -29,11 +41,18 @@ public class HeroGUI : MonoBehaviour {
     // Use this for initialization
     void Start () {
         _levelDropdown.gameObject.SetActive(false);
-	}
+        HearthStoneButtonImg = HearthStoneButton.GetComponent<Image>();
+        Ability1ButtonImg = Ability1Button.GetComponent<Image>();
+        Ability2ButtonImg = Ability2Button.GetComponent<Image>();
+        HealthFillImg = _HealthSlider.transform.FindChild("Fill Area").FindChild("Fill").GetComponent<Image>();
+    }
 
-	// Update is called once per frame
-	void Update () {
+// Update is called once per frame
+void Update () {
         _HealthSlider.value = _localHero.GetCurrentHealth();
+        HealthFillColor.r = (_localHero.GetMaxHealth() - _localHero.GetCurrentHealth() / _localHero.GetMaxHealth());
+        HealthFillColor.g = _localHero.GetCurrentHealth() / _localHero.GetMaxHealth();
+        HealthFillImg.color = HealthFillColor;
         _ManaSlider.value = _localHero.GetCurrentMana();
 
         if (Input.GetButtonDown("HearthStone"))
@@ -58,6 +77,8 @@ public class HeroGUI : MonoBehaviour {
                 UpdateAbility2Select();
                 break;
         }
+        CheckCoolDownButtons();
+        CheckUpgradeChoices();
 
     }
 
@@ -82,6 +103,7 @@ public class HeroGUI : MonoBehaviour {
                 Cursor.SetCursor(null, Vector2.zero, CursorMode.Auto);
                 //this.marker.transform.position = info.point;
                 _localHero.ActivateAbility2(info.collider.gameObject);
+                this.status = GUIStatus.NORMAL;
             }
             else
             {
@@ -131,8 +153,6 @@ public class HeroGUI : MonoBehaviour {
                 _localHero.MoveTo(info.point);
             }
         }
-
-        CheckUpgradeChoices();
 
     }
 
@@ -194,6 +214,28 @@ public class HeroGUI : MonoBehaviour {
             //first line in the dropdown is "choose upgrade", so I need to subtract that line from the choice:
             _localHero.ChooseHeroUpgrade(c-1);
             _levelDropdown.gameObject.SetActive(false);
+        }
+    }
+
+    //update the timer value for each of the ability buttons and update their looks accordingly
+    private void CheckCoolDownButtons()
+    {
+        IndicateButton((Time.time - Mathf.Round(Time.time)), HearthStoneButton, HearthStoneButtonImg);
+        IndicateButton(_localHero.GetAbility1CoolDown(), Ability1Button, Ability1ButtonImg);
+        IndicateButton(_localHero.GetAbility2CoolDown(), Ability2Button, Ability2ButtonImg);
+    }
+
+    private void IndicateButton(float secondsLeft, Button b, Image i)
+    {
+        if (secondsLeft < float.Epsilon)
+        {
+            i.fillAmount = 1f; // Mathf.Clamp01((10f-a1)/10f);
+            b.interactable = true;
+        }
+        else
+        {
+            i.fillAmount = Mathf.Clamp01((10f - secondsLeft) / 10f);
+            b.interactable = false;
         }
     }
 }
